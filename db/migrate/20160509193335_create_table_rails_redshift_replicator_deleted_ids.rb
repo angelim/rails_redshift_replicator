98d71a0106799e1232e9fbf7f8b2671169a104b4
Alexandre Angelim <angelim@angelim.com.br>
2016-05-10 01:51:55 -0300

    fix: adds index to source_table on deleted_ids table

--- db/migrate/20160509193335_create_table_rails_redshift_replicator_deleted_ids.rb
+++ db/migrate/20160509193335_create_table_rails_redshift_replicator_deleted_ids.rb
@@ -1,7 +1,7 @@
 class CreateTableRailsRedshiftReplicatorDeletedIds < ActiveRecord::Migration
   def change
     create_table :rails_redshift_replicator_deleted_ids, id: false do |t|
-      t.string :source_table
+      t.string :source_table, index: true
       t.integer :object_id
     end
   end
